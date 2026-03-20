d4cea953b9cb75999d76f877b3065507ca722585
Austin Grandstaff <200152396+discover-Austin@users.noreply.github.com>
2026-03-19 20:19:26 -0400

    Bump release version to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.tradesketch.estimator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.0.14"
+        versionCode = 17
+        versionName = "1.0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
